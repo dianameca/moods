06445332e6293077573421d8e82e6b90a30c3666
Diana Meca <dianameca@protonmail.com>
2024-11-02 17:11:45 -0400

    Refactor keeping ui unchanged
    # Conflicts:
    #	countdown.js

--- countdown.js
+++ countdown.js
@@ -1,176 +1,176 @@
-var started = 0;
-/* pause by default */
-document.querySelector('#toggleAudio').innerHTML = "PLAY";
-document.querySelector('#selected-mood').innerHTML = "Choose a mood, then press" 
-                                          + "<span style=\"color:red;font-size:120%;\">" 
-                                          + " PLAY</span>";
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("script loaded");
 
-var m = 1;
-document.querySelector('#mode').addEventListener("click", function() {
-  if(m % 3 === 0) {
-    document.querySelector('#mode').innerHTML = "☀️";
-  } else if (m % 3 === 1) {
-    document.querySelector('#mode').innerHTML = "🌈";
-  } else if (m % 3 === 2) {
-    document.querySelector('#mode').innerHTML = "🌑";
+  // state variables
+  let currentModeIndex = 0; // to cycle through mood icons
+  let animationToggleCount = 0;
+  let currentAudio = null; // holds current audio
+  let isAutoPlayEnabled = false; // flag to see if playback is enabled after the first press
+
+  // html element references
+  const audioToggleButton = document.querySelector('#toggleAudio');
+  const moodDisplay = document.querySelector('#selected-mood');
+  const modeToggleButton = document.querySelector('#mode');
+  const hideStickmanButton = document.querySelector('#hide');
+  const countdownDisplayElement = document.querySelector("#countdown");
+
+  // initial ui
+  audioToggleButton.innerHTML = "PLAY"; // set initial button text to PLAY
+  moodDisplay.innerHTML = "Choose a mood, then press <span style='color:red;font-size:120%;'>PLAY</span>";
+
+  // update the current mode displayed on the mode button
+  function updateCurrentMode() {
+      const moodIcons = ["☀️", "🌈", "🌑"];
+      // update button text with the next mood icon
+      modeToggleButton.innerHTML = moodIcons[currentModeIndex % moodIcons.length];
+      currentModeIndex++; // increment for the next button press
   }
-  ++m;
+  modeToggleButton.addEventListener("click", () => {
+      console.log("mode toggle button clicked");
+      updateCurrentMode(); // update mode
+  });
+
+  // toggle stickman animations based on button presses
+  function toggleStickmanAnimation() {
+      console.log("hide stickman button clicked");
+      // select all the parts
+      const stickmanParts = document.querySelectorAll('#head, #torso, #leftleg, #rightleg, #rightarm, #leftarm, #rightfoot, #leftfoot');
+      animationToggleCount++; // increment with each button press
+
+      // 1st toggle - add animations to arms and hide feet
+      if (animationToggleCount === 1) {
+          document.querySelector('#rightarm').classList.add("click1R"); // animate right arm
+          document.querySelector('#leftarm').classList.add("click1L"); // animate left arm
+          document.querySelector('#rightfoot').classList.add("none");
+          hideStickmanButton.innerHTML += "!";
+
+      // 2nd toggle - change animation and add to legs
+      } else if (animationToggleCount === 2) {
+          document.querySelector('#rightarm').classList.replace("click1R", "none"); // right arm still
+          document.querySelector('#leftarm').classList.replace("click1L", "none"); // left arm still
+          document.querySelector('#leftleg').classList.add("click1L"); // animate left leg
+          document.querySelector('#leftfoot').classList.add("click1L"); // animate left foot
+          document.querySelector('#rightfoot').classList.add("none");
+          hideStickmanButton.innerHTML += "!";
+
+      // 2nd+ toggle: display dove
+      } else if (animationToggleCount > 2) {
+          document.querySelector('#stickman').innerHTML = 
+              "<div style='margin-top: 40px;font-size: 15px;'>Okay 😅</div>" +
+              "<img src='assets/img/dove.svg'>";
+      }
+  }
+  // attach event listener to hide button for toggling animations
+  hideStickmanButton.addEventListener("click", toggleStickmanAnimation);
+
+  // stop audio, reset audio state
+  function stopAudio() {
+      if (currentAudio) {
+          currentAudio.pause();
+          audioToggleButton.innerHTML = "PLAY";
+      }
+  }
+
+  
+  function updateMoodDisplay(title, footer) {
+      moodDisplay.innerHTML = `<u>MOOD</u><br>${title}`; 
+      document.querySelector('footer').innerHTML = footer;
+  }
+
+  // play the audio for the selected mood
+  function playMoodAudio(mood) {
+      if (!mood) {
+          console.log("no mood selected");
+          moodDisplay.innerHTML = "Please choose a mood before pressing PLAY.";
+          return; // exit early if no mood selected
+      }
+      stopAudio();
+      const moodConfigurations = {
+          nostalgic: { title: "Nostalgic", footer: "<!--Compact - Fata din vis &nbsp • &nbsp Vama Veche - Epilog-->" },
+          party: { title: "Party", footer: "Ms. Triniti - Let's Celebrate" },
+          pensive: { title: "Pensive", footer: "Neil Diamond - Heart of Gold" },
+          beachy: { title: "Beachy", footer: "Bob Marley and the Wailers - No Woman, No Cry" },
+          motivated: { title: "Motivated", footer: "Michael Jackson - Man in the Mirror" },
+          happy: { title: "Happy", footer: "King Harvest - Dancing in the Moonlight" },
+          romantic: { title: "Romantic", footer: "Cyndi Lauper - Time After Time" },
+          doowop: { title: "Doo wop", footer: "Gene Chandler - Duke Of Earl &nbsp • &nbsp Dion - Runaround Sue" },
+          taylor: { title: "Taylor Swift", footer: "Taylor Swift - All Too Well" },
+          soulful: { title: "Soulful", footer: "Aretha Franklin - I Say A Little Prayer &nbsp • &nbsp Ben E. King - Stand by Me" },
+          rockandroll: { title: "Rock 'n roll", footer: "Fats Domino - Blueberry Hill &nbsp • &nbsp Little Richard - Good Golly, Miss Molly" },
+          hopeful: { title: "Hopeful", footer: "Louis Armstrong - What A Wonderful World" }
+      };
+
+      const moodDetails = moodConfigurations[mood]; // get mood details based on selection
+      if (moodDetails) {
+          currentAudio = new Audio(`assets/audio/${mood}`);
+          updateMoodDisplay(moodDetails.title, moodDetails.footer);
+          if (isAutoPlayEnabled) {
+              currentAudio.play(); // play the audio automatically if enabled
+              audioToggleButton.innerHTML = "PAUSE"; // change button text to PAUSE when playing
+          }
+      }
+  }
+
+  // play/pause listener for the audio toggle button
+  audioToggleButton.addEventListener("click", () => {
+      console.log("audio toggle button clicked");
+      if (currentAudio) {
+          if (currentAudio.paused) { // if audio is currently paused, play it
+              currentAudio.play();
+              audioToggleButton.innerHTML = "PAUSE";
+              isAutoPlayEnabled = true; // enable auto play after the first press
+          } else { // if audio is currently playing, stop
+              stopAudio();
+              isAutoPlayEnabled = false;
+          }
+          currentAudio.loop = true; // loop audio indefinitely
+      }
+  });
+
+  // set up mood button click listeners to play corresponding audio
+  document.querySelectorAll("#moods button").forEach(button => {
+      button.addEventListener("click", () => {
+          console.log(`${button.id} mood button clicked`);
+          playMoodAudio(button.id);
+      });
+  });
+
+  // update countdown display until the new year
+  function updateCountdownDisplay() {
+      const nextYear = new Date().getFullYear() + 1; // get the next year
+      const nextNewYear = new Date(nextYear, 0, 1, 1, 0, 0); // set date for the next new year
+      const currentTime = new Date().getTime(); // get current time in milliseconds
+      const timeRemaining = nextNewYear - currentTime; // calculate time remaining until the new year
+
+      // calculate days, hours, minutes, and seconds remaining
+      const daysLeft = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+      const hoursLeft = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutesLeft = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+      const secondsLeft = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    // format countdown dispaly
+    countdownDisplayElement.innerHTML = `
+        <div style="text-align: center; text-decoration: underline;">Countdown to ${nextYear}</div>
+        <table style="margin: auto; text-align: left;">
+        <br>
+            <tr>
+                <td class="number">${daysLeft}</td>
+                <td class="label">Days</td>
+            </tr>
+            <tr>
+                <td class="number">${hoursLeft}</td>
+                <td class="label">Hours</td>
+            </tr>
+            <tr>
+                <td class="number">${minutesLeft}</td>
+                <td class="label">Minutes</td>
+            </tr>
+            <tr>
+                <td class="number">${secondsLeft}</td>
+                <td class="label">Seconds</td>
+            </tr>
+        </table>
+    `;
+  }
+  setInterval(updateCountdownDisplay, 1000); // update display every second
 });
-
-/* toggle stick man animations */
-var counter = 0;
-document.querySelector('#hide').addEventListener("click", function() {
-  var bonhomme = document.querySelectorAll('#head, #torso, #leftleg, #rightleg, #rightarm, #leftarm, #rightfoot, #leftfoot');
-  counter += 1;
-  if(counter === 1) {
-    document.querySelector('#rightarm').classList.add("click1R");
-    document.querySelector('#leftarm').classList.add("click1L");
-    document.querySelector('#rightfoot').classList.add("none");
-    document.querySelector('#hide').innerHTML += "!";
-  } else if(counter === 2) {
-    document.querySelector('#rightarm').classList.replace("click1R", "none");
-    document.querySelector('#leftarm').classList.replace("click1L", "none");
-    //document.querySelector('#rightleg').classList.add("click2R");
-    document.querySelector('#leftleg').classList.add("click1L");
-    //document.querySelector('#rightfoot').classList.add("click2R");
-    document.querySelector('#leftfoot').classList.add("click1L");
-    document.querySelector('#rightfoot').classList.add("none");
-    //document.querySelector('#head').classList.add("click2R");
-    //document.querySelector('#torso').classList.add("click2R");
-    document.querySelector('#hide').innerHTML += "!";
-  } else if(counter > 2) {
-    document.querySelector('#bonhomme').innerHTML = 
-                            "<div style = \"margin-top: 40px;font-size: 15px;\">Okay 😅</div>"
-                            + "<img src=\"assets/img/dove.svg\"></img>";
-  }
-});
-
-var mood;
-var audio;
-function reset() {
-  audio.pause();
-  document.querySelector('#toggleAudio').innerHTML = "PLAY";
-}
-
-function playMood(mood) {
-  let path = "assets/audio/" + mood;
-  let title =  "<u>MOOD</u><br>";
-  if (started) {
-    reset();
-  }
-  switch(mood) {
-        case 'nostalgic':
-          audio = new Audio(path);
-          document.querySelector('#selected-mood').innerHTML = title + "Nostalgic";
-          document.querySelector('footer').innerHTML = "<!--Compact - Fata din vis • Vama Veche - Epilog-->";
-          break;
-        case 'party':
-          audio = new Audio(path);
-          document.querySelector('#selected-mood').innerHTML = title + "Party";
-          document.querySelector('footer').innerHTML = "Ms. Triniti - Let's Celebrate";
-          break;
-        case 'pensive':
-          audio = new Audio(path);
-          document.querySelector('#selected-mood').innerHTML = title + "Pensive";
-          document.querySelector('footer').innerHTML = "Tina Turner & Eros Ramazzotti - Cose Della Vita";
-          break;
-        case 'beachy':
-          audio = new Audio(path);
-          document.querySelector('#selected-mood').innerHTML = title + "Beachy";
-          document.querySelector('footer').innerHTML = "UB40 - Red Red Wine";
-          break;
-        case 'motivated':
-          audio = new Audio(path);
-          document.querySelector('#selected-mood').innerHTML = title + "Motivated";
-          document.querySelector('footer').innerHTML = "Michael Jackson - Man in the Mirror";
-          break;
-        case 'happy':
-          audio = new Audio(path);
-          document.querySelector('#selected-mood').innerHTML = title + "Happy";
-          document.querySelector('footer').innerHTML = "King Harvest - Dancing in the Moonlight";
-          break;
-        case 'romantic':
-          audio = new Audio(path);
-          document.querySelector('#selected-mood').innerHTML = title + "Romantic";
-          document.querySelector('footer').innerHTML = "Cyndi Lauper - Time After Time";
-          break;
-        case 'doowop':
-          audio = new Audio(path);
-          document.querySelector('#selected-mood').innerHTML = title + "Doo wop";
-          document.querySelector('footer').innerHTML = "Dion - Runaround Sue";
-          break;
-        case 'taylor':
-          audio = new Audio(path);
-          document.querySelector('#selected-mood').innerHTML = title + "Taylor Swift";
-          document.querySelector('footer').innerHTML = "Taylor Swift - All Too Well";
-          break;
-        case 'soulful':
-          audio = new Audio(path);
-          document.querySelector('#selected-mood').innerHTML = title + "Soulful";
-          document.querySelector('footer').innerHTML = "Aretha Franklin - I Say A Little Prayer";
-        break;
-        case 'rockandroll':
-          audio = new Audio(path);
-          document.querySelector('#selected-mood').innerHTML = title + "Let's rock";
-          document.querySelector('footer').innerHTML = "Elvis Presley - Jailhouse Rock";
-        break;
-        case 'hopeful':
-          audio = new Audio(path);
-          document.querySelector('#selected-mood').innerHTML = title + "Hopeful";
-          document.querySelector('footer').innerHTML = "Louis Armstrong - What A Wonderful World";
-        break;
-        default:
-          break;
-  }
-  ++started;
-};
-
-/* play/pause listener */
-document.querySelector('#toggleAudio').addEventListener("click", function() {
-  if (audio.paused) {
-    audio.play();
-    document.querySelector('#toggleAudio').innerHTML = "PAUSE";
-  } else {
-    reset();
-  }
-  audio.loop = true;
-});
-
-/* countdown */
-var nextYear = new Date().getFullYear() + 1;
-var nextNewYear = new Date(nextYear, 0, 1, 1, 0, 0);
-
-var i = setInterval(function() {
-  var now = new Date().getTime();
-  var d = nextNewYear - now;
-  var days = Math.floor(d/(1000*60*60*24));
-  var hours = Math.floor((d%(1000*60*60*24))/(1000*60*60));
-  var minutes = Math.floor((d%(1000*60*60))/(1000*60));
-  var seconds = Math.floor((d%(1000*60))/1000);
-  document.querySelector("#countdown").innerHTML = 
-                    "<u>Countdown to " + nextYear + "</u>" 
-                    + "<br><br>" 
-                    + "<table class=\"center\">"
-                        + "<tr>" 
-                            + "<th>" + days + "</th>" 
-                            + "<th>day(s)</th>"
-                        + "</tr>"
-                        + "<tr>"
-                            + "<th>" + hours + "</th>"
-                            + "<th>hour(s)</th>"
-                        + "</tr>" 
-                        + "<tr>"
-                            + "<th>" + minutes + "</th>"
-                            + "<th>minute(s)</th>"
-                        + "</tr>" 
-                        + "<tr>" 
-                            + "<th>" + seconds + "</th>"
-                            + "<th>second(s)</th>"
-                        + "</tr>"
-                    + "</table>";
-  if (d < 0) {
-    clearInterval(i);
-    document.querySelector('#countdown').innerHTML = 
-                      "We made it!<br>Happy New Year 🎊 💖";
-    }
-}, 1000);
